e9ce3417b83bdff8d101bf5de2beb75aa90384bc
Saravanan Gowthaman <sarav200898@gmail.com>
2019-10-17 23:01:10 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,16 @@
-#empty
+with open('mails.txt') as f:
+    original = f.read().splitlines()
+    
+print(len(original))
+with open('temp.txt') as f:
+    temp = f.read().splitlines()
+print(len(temp))
+    
+final=set(original+temp)
+fin_list=list(final)
+print(len(fin_list))
+
+with open('mails.txt', 'w') as f:
+    for item in fin_list:
+        f.write("%s\n" % item)
+        
